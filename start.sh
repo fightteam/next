035354f477510fee38272d54405449626964f77b
excalibur <excalibur-faith@qq.com>
2014-09-18 22:52:34 +0800

    add shell

--- start.sh
+++ start.sh
@@ -5,22 +5,6 @@
 # 
 
 
-# Start mysql
-# if [ -f /etc/nginx/nginx ]; then
-#     mysqld_safe
-# fi
-
-# Start redis
-if [ -f /etc/redis/redis.conf ]; then
-    redis-server /etc/redis/redis.conf &
-fi
-
-# Start tomcat 
-
-# Start nginx
-if [ -f /etc/nginx/nginx ]; then
-    nginx
-fi
 
 # start all the services
 /usr/local/bin/supervisord -n
